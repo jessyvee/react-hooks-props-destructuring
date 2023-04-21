94750ff54d2cfaa6e87383aa5d67af28b426e397
Jesse <jessyvee1@gmail.com>
2023-04-21 14:12:25 +0300

    asignment done

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -5,6 +5,8 @@ function MovieCard({
   posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
   genres,
 }) {
+
+  // function return
   return (
     <div className="movie-card">
       <img src={posterSrc} alt={title} />
